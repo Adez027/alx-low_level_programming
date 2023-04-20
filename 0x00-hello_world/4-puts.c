ea1209acc46d1e2bc02ce26a80d1c4dcc226cbcd
Adez027 <bellmum2015@gmail.com>
2023-04-20 07:10:06 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.>
-/*
- * main - print a line of code using puts
+/**
+ * main -Print a line of code using puts
  *
- * return :0
-*/ int main(void)
+ * Return: 0
+*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
