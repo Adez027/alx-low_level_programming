1839bfa6b32504172286b468df76120195dde898
Adez027 <bellmum2015@gmail.com>
2023-04-20 14:23:15 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistdo.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return 1(Success) 
+ * Return 1 (Success) 
 */
 int main(void)
 {
-	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	retun (1);
+	fprint("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stdout);
+	retun(1);
 }
