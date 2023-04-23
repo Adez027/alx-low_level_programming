8330be26c77178f48c7867bb9a2309a397215b94
Adez027 <bellmum2015@gmail.com>
2023-04-23 09:41:33 -0700

    Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - tebahpla
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 122; s >= 97; s--)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
