55f4f1aa1eb8b034cf1ac85a2f2cec6d5e29c560
Adez027 <bellmum2015@gmail.com>
2023-05-02 23:58:35 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+/**
+ * _puts - prints a string to stdout
+ * @str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int e = 0;
+
+	while (str[e] != '\0')
+	{
+		_putchar(str[e]);
+		e++;
+	}
+	_putchar('\n');
+}
+
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
