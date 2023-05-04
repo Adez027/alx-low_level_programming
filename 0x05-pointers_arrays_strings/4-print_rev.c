7162caf461c520ddbed75278c8b2e918c6c0c1f1
Adez027 <bellmum2015@gmail.com>
2023-05-03 18:18:06 -0700

    Puts

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,21 +7,21 @@
  */
 void print_rev(char *s)
 {
-	int longi;
-	int c = 0;
+	int longi = 0;
+	int o;
 
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
-
 	}
 	s--;
 
-	for (c = longi; c > 0; c--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
