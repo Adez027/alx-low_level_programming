b3aa69cbd174fd6003cd483702e0d6440263709d
Adez027 <bellmum2015@gmail.com>
2023-05-16 13:11:04 -0700

    Malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,10 +9,10 @@
  */
 int count_word(char *s)
 {
-	int flag, c, w;
+	int flag, c, z;
 
 	flag = 0;
-	w = 0;
+	z = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
@@ -21,23 +21,22 @@ int count_word(char *s)
 		else if (flag == 0)
 		{
 			flag = 1;
-			w++;
+			z++;
 		}
 	}
-	return (w);
+	return (z);
 }
 
 /**
  * **strtow - splits a string into words
  * @str: string to split
  *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * Return: pointer to an array of strings (Success) or NULL (Error)
  */
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
+	int q, r = 0, len = 0, words, p = 0, start, end;
 
 	while (*(str + len))
 		len++;
@@ -46,32 +45,32 @@ char **strtow(char *str)
 	if (words == 0)
 		return (NULL);
 
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	matrix = (char **)malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len; i++)
+	for (q = 0; q <= len; q++)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		if (str[q] == ' ' || str[q] == '\0')
 		{
-			if (c)
+			if (p)
 			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				end = q;
+				tmp = (char *)malloc(sizeof(char) * (p + 1));
 				if (tmp == NULL)
 					return (NULL);
 
 				while (start < end)
 					*tmp++ = str[start++];
-				tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
+				*tmp = '\0';
+				matrix[r] = tmp - p;
+				r++;
+				p = 0;
 			}
 		}
-		else if (c++ == 0)
-			start = i;
+		else if (p++ == 0)
+			start = q;
 	}
-	matrix[k] = NULL;
+	matrix[r] = NULL;
 	return (matrix);
 }
